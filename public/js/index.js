a04ea6236a7059a9bcc868a5e9aee6912426710b
Francisco Quijada <ezefranquijada@gmail.com>
2023-02-15 12:37:25 -0400

    fege

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,17 @@
 
 const toggleBtn = document.querySelector('#toggle-btn');
 const menu = document.querySelector('.menu-listm');
-
+const header = document.querySelector('.header');
 
 toggleBtn.addEventListener( 'click', (e) => {
     e.preventDefault()
     menu.classList.toggle('menu-active')
     console.log('click');
+
+    // if (header.classList.contains('header-nav')){
+    //     header.classList.remove('header-nav')
+    // } else {
+    //     header.classList.add('header-nav')
+    // }
+
 } )
